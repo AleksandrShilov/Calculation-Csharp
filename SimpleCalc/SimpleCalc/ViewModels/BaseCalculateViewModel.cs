2e8191652722c759b1ffcec41ffbeed39ba5f7f8
wilfordd <scrib.ru@mail.ru>
2024-07-31 22:15:58 +0700

    add one eventhandler

--- SimpleCalc/SimpleCalc/ViewModels/BaseCalculateViewModel.cs
+++ SimpleCalc/SimpleCalc/ViewModels/BaseCalculateViewModel.cs
@@ -1,4 +1,43 @@
+using ReactiveUI;
+using System;
+using System.Reactive;
+
 namespace SimpleCalc.ViewModels;
 
 
-public class BaseCalculateViewModel : ViewModelBase { }
+public class BaseCalculateViewModel : ViewModelBase 
+{
+    private string _displayStr = "0";
+
+    public ReactiveCommand<int, Unit> AddNumberCommand { get; }
+    public ReactiveCommand<string, Unit> RemoveLastNumberCommand { get; }
+
+    public BaseCalculateViewModel()
+    {
+        AddNumberCommand = ReactiveCommand.Create<int>(AddNumber);
+        RemoveLastNumberCommand = ReactiveCommand.Create<string>(RemoveLastNumber);
+    }
+
+    private void AddNumber(int value)
+    {
+        switch(value)
+        {
+            case 1:
+                ShownValue += "1";
+                break;
+        }
+    }
+
+    public void RemoveLastNumber(string value)
+    {
+        ShownValue = ShownValue;
+    }
+
+    public string ShownValue
+    {
+        get => _displayStr;
+        set => this.RaiseAndSetIfChanged(ref _displayStr, value);
+    }
+
+
+}
