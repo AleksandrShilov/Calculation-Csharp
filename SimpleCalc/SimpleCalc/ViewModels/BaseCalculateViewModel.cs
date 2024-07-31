cd3638513623bb9b0fcc7f1dbb10c41651f6a8c4
wilfordd <scrib.ru@mail.ru>
2024-07-31 21:19:25 +0700

    attribute

--- SimpleCalc/SimpleCalc/ViewModels/BaseCalculateViewModel.cs
+++ SimpleCalc/SimpleCalc/ViewModels/BaseCalculateViewModel.cs
@@ -1,4 +1,46 @@
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace SimpleCalc.ViewModels;
 
+public partial class BaseCalculateViewModel : ViewModelBase
+{
+    public BaseCalculateViewModel()
+    {
+        // Инициализация команды
+        OnButtonClickedCommand = new RelayCommand(OnButtonClicked);
+    }
 
-public class BaseCalculateViewModel : ViewModelBase { }
+    // Команда для кнопки
+    public RelayCommand OnButtonClickedCommand { get; }
+
+    [ObservableProperty]
+    private string _yourProperty;
+
+    private void OnButtonClicked()
+    {
+        // Ваш код обработки нажатия кнопки
+        YourProperty = _yourProperty + "7";
+    }
+
+    public void AddNumber(int value)
+    {
+
+    }
+}
+
+//public class BaseCalculateViewModel : ViewModelBase 
+//{
+//    private double _secondValue;
+
+//    public double ShownValue
+//    {
+//        get => _secondValue;
+//        set => this.RaiseAndSelfChanged(ref _secondValue, value);
+//    }
+
+//    public void AddNumber(int value)
+//    {
+
+//    }
+//}
